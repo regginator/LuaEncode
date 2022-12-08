5f477ee11d3f83c315c3c8ab745dbe8cc0fd4e11
LuaPhysics <52427945+LuaPhysics@users.noreply.github.com>
2022-12-08 13:41:48 -0600

    Update LuaEncode.lua

--- src/LuaEncode.lua
+++ src/LuaEncode.lua
@@ -226,9 +226,9 @@ local function LuaEncode(inputTable, options)
                 if Success then
                     return ErrorOrValue, true
                 else
-                    local Padding = "" do
+                    local Padding = "=" do
                         ErrorOrValue:gsub("%](=*)%]", function(match)
-                            if match == Padding then
+                            if match >= Padding then
                                 Padding = match .. "="
                             end
                         end)
