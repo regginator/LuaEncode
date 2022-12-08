3830f0d9bdc28dc19a4e8168d420b4bcc8fc4f7e
regginator <reggie@latte.to>
2022-12-08 00:20:51 -0600

    (Encoding typo) Add fix for DockWidgetPluginGuiInfo, bump version

--- src/LuaEncode.lua
+++ src/LuaEncode.lua
@@ -226,7 +226,10 @@ local function LuaEncode(inputTable, options)
                 if Success then
                     return ErrorOrValue, true
                 else
-                    return string.format("DockWidgetPluginGuiInfo.new() -- Error on serialization: %q", ErrorOrValue)
+                    return string.format(
+                        "DockWidgetPluginGuiInfo.new(--[[Error on serialization: %q]])",
+                        string.gsub(ErrorOrValue, "%[*%]*", "") -- Removes all "[" or "]" characters from err string, just for escaping purposes
+                    ), true
                 end
             end
 
