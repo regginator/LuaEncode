410fa41d49fe6b9a042c41ea9d6600786286e66c
reggie <reggie@latte.to>
2024-11-26 22:30:55 -0600

    Update CyclicReference test

--- tests/CyclicReference.lua
+++ tests/CyclicReference.lua
@@ -1,8 +1,17 @@
 --!nocheck
 local LuaEncode = string.sub(_VERSION, 1, 4) == "Luau" and require("../src/LuaEncode") or require("src/LuaEncode")
 
-local Table = {}
-Table[1] = Table
+local Table = {
+    {
+        "Hello",
+        AnotherTable = {},
+        ["This can't be indexed raw"] = {}
+    },
+}
+
+Table.Self = Table
+Table[2] = Table[1].AnotherTable
+Table["3"] = Table[1]["This can't be indexed raw"]
 
 print(LuaEncode(Table, {
     Prettify = true,
